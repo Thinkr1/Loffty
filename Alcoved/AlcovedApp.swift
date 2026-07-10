4cfbc25a8a7c3bb9e3bbf002810d0663d180fdc9
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2026-07-10 11:43:20 +0200

    Initial Commit

---
+++ Alcoved/AlcovedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlcovedApp.swift
+//  Alcoved
+//
+//  Created by Pierre-Louis ML on 10/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AlcovedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alcoved/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alcoved
+//
+//  Created by Pierre-Louis ML on 10/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
